6d3dc9efd2fbaab7aca95e706a51ecee338c309d
Omamowho Phanuel <omamowhophanuel@gmail.com>
2023-03-07 13:35:39 -0800

    0x01-python-if_else_loops_functions

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# Author - Bamidele Adefolaju
+def add(a, b):
+    """Return the addition of a and b."""
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def islower(n):
+    """Function checks for lowercase characters."""
+    if ord(n) >= and ord(n) <= 122:
+        return True
+    else:
+        return False
